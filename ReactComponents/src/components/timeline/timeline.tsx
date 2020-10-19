8a3bf4b47ef43f08b1f935236c01356bf66c6534
Alex <alexsr400@gmail.com>
2020-10-19 15:46:19 +0530

    Timeline UI

--- ReactComponents/src/components/timeline/timeline.tsx
+++ ReactComponents/src/components/timeline/timeline.tsx
@@ -11,76 +11,42 @@ function Timeline() {
 
     const dummyTimelineData: TimelineItem[] = [
         {
-            timestamp: new Date('04-25-2019'),
-            title: 'Latch lock firmware upgraded to 3.0.0',
-            type: 'FIRMWARE_UPGRADE',
+            timestamp: new Date('2020-10-24 10:55:00'),
+            title: '',
+            content: 'Reference value plan transmission',
+            level: 'warn'
         },
         {
-            timestamp: new Date('04-23-2019'),
-            title: 'Problem resolved - Safe malfunction',
-            type: 'SERVICE',
-            status: 'COMPLETED'
+            timestamp: new Date('2020-10-24 12:00:00'),
+            title: 'Start bidding',
+            content: 'DR Announced'
         },
         {
-            timestamp: new Date('04-22-2019'),
-            title: 'Latch lock replaced with #5789123',
-            type: 'PART_CHANGE',
-        },
-        {
-            timestamp: new Date('04-22-2019'),
-            title: 'Service started against complaint - Safe malfunction',
-            type: 'SERVICE',
-            status: 'STARTED'
-        },
-        {
-            timestamp: new Date('04-20-2019'),
-            title: 'Complaint registered - Safe malfunctioning',
-            type: 'SERVICE',
-            status: 'REQUESTED'
-        },
-        {
-            timestamp: new Date('02-14-2019'),
-            title: 'Annual servicing completed',
-            type: 'SERVICE',
-            status: 'COMPLETED'
-        },
-        {
-            timestamp: new Date('06-25-2018'),
-            title: 'Safe inspections and servicing has carried out by service team',
-            type: 'SERVICE',
-            status: 'STARTED'
-        },
-        {
-            timestamp: new Date('02-14-2019'),
-            title: 'Annual servicing',
-            type: 'SERVICE',
-            status: 'REQUESTED'
-        },
-        {
-            timestamp: new Date('02-14-2018'),
-            title: 'Product installed successfully in the specified site'
-        },
-        {
-            timestamp: new Date('02-05-2018'),
-            title: 'Order placed'
-        },
-        {
-            timestamp: new Date('01-15-2018'),
-            title: 'Product available for selling'
-        },
-        {
-            timestamp: new Date('01-01-2018'),
-            title: 'Product Manufactured'
+            timestamp: new Date('2020-10-24 13:00:00'),
+            title: '',
+            content: 'Next week adjustment unit price'
         }
     ];
+
+    const formatDate = (date: Date) => {
+        return date.getMonth() + '/' + date.getDate();
+    }
+
+    const formatTime = (date: Date) => {
+        return date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
+    }
 
     return <div className="timeline-wrap">
         <div className="timeline">
             {dummyTimelineData.map((tlItem, index) => <div key={index} className="right timeline-item visible">
+                <div className="time-section">
+                    <div className="date">{formatDate(tlItem.timestamp)}</div>
+                    <div className="time">{formatTime(tlItem.timestamp)}</div>
+                </div>
                 <div className="content">
-                    <div className="timeline-title">Title</div>
+                    <div className={`timeline-title ${tlItem.level}`}>{tlItem.title}</div>
                     <div className="main-content">
-                        <div style={{ marginBottom: '10px' }}>{tlItem.title}</div>
+                        <div style={{ marginBottom: '10px' }}>{tlItem.content}</div>
                     </div>
                 </div>
             </div>)}
--- ReactComponents/src/data/models/timeline-item.ts
+++ ReactComponents/src/data/models/timeline-item.ts
@@ -1,7 +1,9 @@
 export interface TimelineItem {
     timestamp: Date;
     title: string;
+    content: string;
     type?: string;
     status?: string;
     metadata?: any;
+    level?: string;
 }
